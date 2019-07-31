175c3eb33be2f2d33eda8078f94932901bafbabf
Alex <almeidafer92@gmail.com>
2019-07-30 23:24:05 -0300

    Altereções em config.properties e ConnectionFactory

--- src/main/java/com/needhotel/modelo/conexao/ConnectionFactory.java
+++ src/main/java/com/needhotel/modelo/conexao/ConnectionFactory.java
@@ -1,5 +1,8 @@
+
 package com.needhotel.modelo.conexao;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,18 +10,33 @@ import java.util.Properties;
 
 public class ConnectionFactory {
 
-    public static String url = "jdbc:postgresql://localhost:5432/needHotel";
+    public static String url;
 
-    public static String user = "postgres";
+    public static String user;
 
-    public static String password = "1234";
+    public static String password;
+
+    public static String driver;
+
+    static {
+        InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(inputStream);
+            url = properties.getProperty("conexao.url");
+            user = properties.getProperty("conexao.usuario");
+            password = properties.getProperty("conexao.senha");
+            driver = properties.getProperty("conexao.driver");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public Connection getConnection() {
         Connection connection = null;
 
         try {
-            Class.forName("org.postgresql.Driver");
+            Class.forName(driver);
             connection = DriverManager.getConnection(url, user, password);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
