85e7ca8b7fab5d59d9bd78a764ceb63c1d2e70f2
Alex <almeidafer92@gmail.com>
2019-08-02 01:04:40 -0300

    Composition de Login e Home Finalizados

---
+++ src/main/java/com/needhotel/LoginBean.java
@@ -0,0 +1,31 @@
+package com.needhotel;
+
+
+import com.needhotel.modelo.dao.implementacao.UsuarioDaoImpl;
+import com.needhotel.modelo.domain.Usuario;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.view.facelets.Facelet;
+import javax.servlet.http.HttpSession;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean {
+
+    private String email;
+    private String senha;
+
+    private UsuarioDaoImpl usuarioDao = new UsuarioDaoImpl();
+
+    private String login(){
+//        Usuario usuario = usuarioDao.autenticacao(email, senha);
+
+//        if (usuario != null){
+//            HttpSession session = Facelet.getContext()
+            return "home.xhtml";
+//        }else {
+//            return "";
+//        }
+    }
+}
